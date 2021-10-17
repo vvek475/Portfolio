47a56b7292006d41c64079bdcb75601331591ceb
vivek <vickfreeindia@gmail.com>
2021-10-17 17:42:51 +0530

    initial commit

--- JS/script.js
+++ JS/script.js
@@ -6,27 +6,22 @@ console.log(home1,home2[0])
 console.log(home2[0])
 homesel=document.getElementById('home')
 home1[0].addEventListener('click',function(e){
-    console.log('ok')
     calc()
     home2[0].style.display="flex"
 })
 home1[1].addEventListener('click',function(e){
-    console.log('ok')
     calc()
     home2[1].style.display="flex"
 })
 home1[2].addEventListener('click',function(e){
-    console.log('ok')
     calc()
     home2[2].style.display="flex"
 })
 home1[3].addEventListener('click',function(e){
-    console.log('ok')
     calc()
     home2[3].style.display="flex"
 })
 home1[4].addEventListener('click',function(e){
-    console.log('ok')
     calc()
     home2[4].style.display="flex"
 })
@@ -36,3 +31,9 @@ function calc(){
         element.style.display='none'
     });
 }
+var dropbtn=document.querySelector(".dropbtn")
+var overlay=document.querySelector(".overlay")
+console.log(overlay,dropbtn)
+dropbtn.addEventListener('click',function(e){
+    overlay.classList.toggle('navbar-slider');
+})
