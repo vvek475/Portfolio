624c7411fbcff934bdc217e9101a7c75773bc330
vivek <vickfreeindia@gmail.com>
2021-10-24 00:50:00 +0530

    initial commit

--- JS/script.js
+++ JS/script.js
@@ -104,3 +104,36 @@ theme.addEventListener('click',function(e){
     bg.classList.toggle('bg_theme')
 })
 bg.style.backgroundSize='cover'
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+  keyboard: {
+    enabled: true,
+  },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+      },
+      1200: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+      },
+    },
+  });
